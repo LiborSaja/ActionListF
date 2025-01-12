cdd8f93e16fc6728c859f7dc01082c7ecd6d6e49
LiborSaja <sajalibor@gmail.com>
2025-01-12 20:31:21 +0100

    updated_url_for_docker_in_axiosjs

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
-// instance axios, nastavuje url backendu, čas čekání a hlavičky - typ obsahu, který bude poslán na server
-const axiosInstance = axios.create({
-    baseURL: "https://localhost:7021/api",
-    timeout: 10000,
-    headers: {
-        "Content-Type": "application/json",
-    },
-});
-
-// url pro docker
+// instance axios, nastavuje url backendu, čas čekání a hlavičky - typ obsahu, který bude poslán na server(lokální)
 // const axiosInstance = axios.create({
-//     baseURL: "http://localhost:8080/api",
+//     baseURL: "https://localhost:7021/api",
 //     timeout: 10000,
 //     headers: {
 //         "Content-Type": "application/json",
 //     },
 // });
+
+// url pro docker
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:8080/api",
+    timeout: 10000,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
 export default axiosInstance;
